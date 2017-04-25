0afa19fce50cf0b04c4bfcd181f8574d12a8abc4
Jongyeol Baek <geeksbaek@gmail.com>
2017-04-25 11:55:24 +0900

    working with char, exps

--- charactor.go
+++ charactor.go
@@ -1,1 +1,90 @@
 package archeage
+
+type Character struct {
+	Name              string
+	UUID              string
+	Server            string
+	Level             int
+	Nation            string
+	Expedition        *Expedition
+	ExpeditionHistory []*Expedition
+	Stat              *Stat
+	Equipments        *Equipments
+	Class             *Class
+	Actabilities      []*Actabilitie
+	EastLanguage      int
+	WestLanguage      int
+}
+
+type Stat struct {
+	Health   int // 생명력
+	Vitality int // 활력
+
+	Strength     int // 힘
+	Spirit       int // 정신
+	Intelligence int // 지능
+	Stamina      int // 체력
+	Agility      int // 민첩
+
+	AttackSpeed float64 // 공격 속도(%)
+
+	// 기본
+	Defense      int // 물리 방어도
+	MagicDefense int // 마법 저항도
+}
+
+type Equipments struct {
+	Costume    *Equipment
+	InnerArmor *Equipment
+
+	Head   *Equipment
+	Chest  *Equipment
+	Waist  *Equipment
+	Wrists *Equipment
+	Hands  *Equipment
+	Legs   *Equipment
+	Feet   *Equipment
+
+	Cloak *Equipment
+
+	Hand1Weapon *Equipment
+	Hand2Weapon *Equipment
+	Shield      *Equipment
+	Bow         *Equipment
+	Instruments *Equipment
+
+	Necklace *Equipment
+	Earring1 *Equipment
+	Earring2 *Equipment
+	Ring1    *Equipment
+	Ring2    *Equipment
+}
+
+type Equipment struct {
+	Name  string
+	Grade string
+	Score int
+}
+
+type Class struct {
+	Name   string
+	Skills []*Skills
+}
+
+type Skills struct {
+	Level       int
+	IsHatred    bool
+	HatredLevel int
+	SkillList   []string
+}
+
+type Actabilitie struct {
+	Name       string
+	Level      int
+	Score      int
+	Percentage int
+}
+
+func (a *ArcheAge) SearchCharactor(server, name string) (c Character, err error) {
+	return
+}
--- expedition.go
+++ expedition.go
@@ -1,1 +1,87 @@
 package archeage
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type Expedition struct {
+	Name      string
+	URL       string
+	Server    string
+	Number    string
+	Nation    string
+	Thumbnail string
+	Level     int
+	Master    *Character
+	Members   []*Character
+	People    int
+}
+
+type Expeditions []Expedition
+
+const (
+	searchExpeditionFormat = "https://archeage.xlgames.com/search?dt=expeditions&keyword=%s&server=%s"
+)
+
+const (
+	expRowQuery = "ul.lst > li > div.exped_info"
+)
+
+var (
+	expeditionRe = regexp.MustCompile(`expeditions\/(\w+)\/`)
+)
+
+func (a *ArcheAge) FetchAllExpeditions(server string) {
+	expListURL := fmt.Sprintf("https://archeage.xlgames.com/exps/all?gameServer=%s", server)
+
+	doc, err := a.get(expListURL)
+	if err != nil {
+		return
+	}
+
+	expeditions := Expeditions{}
+
+	doc.Find(expRowQuery).Each(func(i int, row *goquery.Selection) {
+		URL, _ := row.Find("a").First().Attr("href")
+		expeditions = append(expeditions, a.fetchSingleExpeditionByURL(URL))
+	})
+}
+
+func (a *ArcheAge) SearchExpedition(server, name string) {
+
+}
+
+func (a *ArcheAge) fetchSingleExpeditionByParam(server, number string) Expedition {
+	URL := fmt.Sprintf("https://archeage.xlgames.com/expeditions/%s/%s", server, number)
+	return a.fetchSingleExpeditionByURL(URL)
+}
+
+func (a *ArcheAge) fetchSingleExpeditionByURL(URL string) (expedition Expedition) {
+	doc, err := a.get(URL)
+	if err != nil {
+		return
+	}
+
+	expedition.Name = doc.Find("span.name").Text()
+	expedition.URL = URL
+	expedition.Server = func() string {
+		if matched := expeditionRe.FindStringSubmatch(URL); len(matched) == 2 {
+			return matched[1]
+		}
+		return "Unknown"
+	}()
+	expedition.Number = regexp.MustCompile(`/\w+/\w+/(\d+)`).FindStringSubmatch(expedition.URL)[1]
+	expedition.Nation = doc.Find("span.nation > em, span.group > em").Text()
+	expedition.Thumbnail, _ = doc.Find("span.exped_thumb > img").Attr("src")
+	expedition.Level, _ = strconv.Atoi(doc.Find("div.sub > span.lv > span.count").Text())
+	// masterUUID, _ := doc.Find("a.character-link").Attr("data-uuid")
+	// expedition.Master = fetchCharacter(masterUUID)
+	// expedition.Members
+	// expedition.People
+
+	return
+}
