be9044ffe31e4717ba40ea039aa289931068def8
Jongyeol Baek <geeksbaek@gmail.com>
2017-04-25 17:52:37 +0900

    impl base of char, exp

--- charactor.go
+++ charactor.go
@@ -1,19 +1,22 @@
 package archeage
 
+import (
+	"fmt"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
 type Character struct {
-	Name              string
-	UUID              string
-	Server            string
-	Level             int
-	Nation            string
-	Expedition        *Expedition
-	ExpeditionHistory []*Expedition
-	Stat              *Stat
-	Equipments        *Equipments
-	Class             *Class
-	Actabilities      []*Actabilitie
-	EastLanguage      int
-	WestLanguage      int
+	Name       string
+	UUID       string
+	Server     string
+	Level      int
+	Nation     string
+	Expedition *Expedition
+	Stat       *Stat
+	// Equipments   *Equipments
+	Class *Class
+	// Actabilities []*Actabilitie
 }
 
 type Stat struct {
@@ -26,45 +29,58 @@ type Stat struct {
 	Stamina      int // 체력
 	Agility      int // 민첩
 
+	Speed       int     // 이동 속도(%)
+	CastTime    float64 // 시전 시간(%)
 	AttackSpeed float64 // 공격 속도(%)
 
 	// 기본
-	Defense      int // 물리 방어도
-	MagicDefense int // 마법 저항도
+	MeleeDPS     float64 // 근접 공격력
+	RangeDPS     float64 // 원거리 공격력
+	MagicDPS     float64 // 주문력
+	HealingPower float64 // 치유력
+	Defense      int     // 물리 방어도
+	MagicDefense int     // 마법 저항도
+	GearScore    int     // 장비점수
+
+	// 공격
+
+	// 방어
+
+	// 회복
 }
 
-type Equipments struct {
-	Costume    *Equipment
-	InnerArmor *Equipment
+// type Equipments struct {
+// 	Costume    *Equipment
+// 	InnerArmor *Equipment
 
-	Head   *Equipment
-	Chest  *Equipment
-	Waist  *Equipment
-	Wrists *Equipment
-	Hands  *Equipment
-	Legs   *Equipment
-	Feet   *Equipment
+// 	Head   *Equipment
+// 	Chest  *Equipment
+// 	Waist  *Equipment
+// 	Wrists *Equipment
+// 	Hands  *Equipment
+// 	Legs   *Equipment
+// 	Feet   *Equipment
 
-	Cloak *Equipment
+// 	Cloak *Equipment
 
-	Hand1Weapon *Equipment
-	Hand2Weapon *Equipment
-	Shield      *Equipment
-	Bow         *Equipment
-	Instruments *Equipment
+// 	Hand1Weapon *Equipment
+// 	Hand2Weapon *Equipment
+// 	Shield      *Equipment
+// 	Bow         *Equipment
+// 	Instruments *Equipment
 
-	Necklace *Equipment
-	Earring1 *Equipment
-	Earring2 *Equipment
-	Ring1    *Equipment
-	Ring2    *Equipment
-}
+// 	Necklace *Equipment
+// 	Earring1 *Equipment
+// 	Earring2 *Equipment
+// 	Ring1    *Equipment
+// 	Ring2    *Equipment
+// }
 
-type Equipment struct {
-	Name  string
-	Grade string
-	Score int
-}
+// type Equipment struct {
+// 	Name  string
+// 	Grade string
+// 	Score int
+// }
 
 type Class struct {
 	Name   string
@@ -78,13 +94,34 @@ type Skills struct {
 	SkillList   []string
 }
 
-type Actabilitie struct {
-	Name       string
-	Level      int
-	Score      int
-	Percentage int
+// type Actabilitie struct {
+// 	Name       string
+// 	Level      int
+// 	Score      int
+// 	Percentage int
+// }
+
+const (
+	charactorURLFormat = "https://archeage.xlgames.com/characters/%s"
+)
+
+func (a *ArcheAge) SearchCharactor(server, name string) (c *Character, err error) {
+	return
 }
 
-func (a *ArcheAge) SearchCharactor(server, name string) (c Character, err error) {
+func (a *ArcheAge) fetchCharactorByUUID(uuid string) (c *Character, err error) {
+	url := fmt.Sprintf(charactorURLFormat, uuid)
+	return a.fetchCharactorByURL(url)
+}
+
+func (a *ArcheAge) fetchCharactorByURL(url string) (c *Character, err error) {
+	doc, err := a.get(url)
+	if err != nil {
+		return
+	}
+	return a.parseCharactor(doc, url)
+}
+
+func (a *ArcheAge) parseCharactor(doc *goquery.Document, url string) (c *Character, err error) {
 	return
 }
--- expedition.go
+++ expedition.go
@@ -1,6 +1,7 @@
 package archeage
 
 import (
+	"errors"
 	"fmt"
 	"regexp"
 	"strconv"
@@ -25,6 +26,7 @@ type Expeditions []Expedition
 
 const (
 	searchExpeditionFormat = "https://archeage.xlgames.com/search?dt=expeditions&keyword=%s&server=%s"
+	expeditionURLFormat    = "https://archeage.xlgames.com/expeditions/%s/%s"
 )
 
 const (
@@ -35,53 +37,38 @@ var (
 	expeditionRe = regexp.MustCompile(`expeditions\/(\w+)\/`)
 )
 
-func (a *ArcheAge) FetchAllExpeditions(server string) {
-	expListURL := fmt.Sprintf("https://archeage.xlgames.com/exps/all?gameServer=%s", server)
+func (a *ArcheAge) SearchExpedition(server, name string) (e *Expedition, err error) {
+	return
+}
 
-	doc, err := a.get(expListURL)
+func (a *ArcheAge) fetchExpeditionByNum(server, number string) (e *Expedition, err error) {
+	url := fmt.Sprintf(expeditionURLFormat, server, number)
+	return a.fetchExpeditionByURL(url)
+}
+
+func (a *ArcheAge) fetchExpeditionByURL(url string) (e *Expedition, err error) {
+	doc, err := a.get(url)
 	if err != nil {
 		return
 	}
-
-	expeditions := Expeditions{}
-
-	doc.Find(expRowQuery).Each(func(i int, row *goquery.Selection) {
-		URL, _ := row.Find("a").First().Attr("href")
-		expeditions = append(expeditions, a.fetchSingleExpeditionByURL(URL))
-	})
+	return a.parseExpedition(doc, url)
 }
 
-func (a *ArcheAge) SearchExpedition(server, name string) {
-
-}
-
-func (a *ArcheAge) fetchSingleExpeditionByParam(server, number string) Expedition {
-	URL := fmt.Sprintf("https://archeage.xlgames.com/expeditions/%s/%s", server, number)
-	return a.fetchSingleExpeditionByURL(URL)
-}
-
-func (a *ArcheAge) fetchSingleExpeditionByURL(URL string) (expedition Expedition) {
-	doc, err := a.get(URL)
-	if err != nil {
-		return
+func (a *ArcheAge) parseExpedition(doc *goquery.Document, url string) (e *Expedition, err error) {
+	e.Name = doc.Find("span.name").Text()
+	e.URL = url
+	if matched := expeditionRe.FindStringSubmatch(e.URL); len(matched) == 2 {
+		e.Server = matched[1]
+	} else {
+		return nil, errors.New("Unknown Server")
 	}
-
-	expedition.Name = doc.Find("span.name").Text()
-	expedition.URL = URL
-	expedition.Server = func() string {
-		if matched := expeditionRe.FindStringSubmatch(URL); len(matched) == 2 {
-			return matched[1]
-		}
-		return "Unknown"
-	}()
-	expedition.Number = regexp.MustCompile(`/\w+/\w+/(\d+)`).FindStringSubmatch(expedition.URL)[1]
-	expedition.Nation = doc.Find("span.nation > em, span.group > em").Text()
-	expedition.Thumbnail, _ = doc.Find("span.exped_thumb > img").Attr("src")
-	expedition.Level, _ = strconv.Atoi(doc.Find("div.sub > span.lv > span.count").Text())
+	e.Number = regexp.MustCompile(`/\w+/\w+/(\d+)`).FindStringSubmatch(e.URL)[1]
+	e.Nation = doc.Find("span.nation > em, span.group > em").Text()
+	e.Thumbnail, _ = doc.Find("span.exped_thumb > img").Attr("src")
+	e.Level, _ = strconv.Atoi(doc.Find("div.sub > span.lv > span.count").Text())
 	// masterUUID, _ := doc.Find("a.character-link").Attr("data-uuid")
 	// expedition.Master = fetchCharacter(masterUUID)
 	// expedition.Members
 	// expedition.People
-
 	return
 }
