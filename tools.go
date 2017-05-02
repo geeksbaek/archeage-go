4e5ed66172eb9116d1faff30fb62eda8504f18e6
Jongyeol Baek <geeksbaek@gmail.com>
2017-05-02 17:05:35 +0900

    add ServerNameMap

--- tools.go
+++ tools.go
@@ -6,6 +6,16 @@ import (
 	"strings"
 )
 
+var ServerNameMap = map[string]string{
+	"안탈론":  "ANTHALON",
+	"크라켄":  "KRAKEN",
+	"노아르타": "NOARTA",
+	"키리오스": "KYRIOS",
+	"누이":   "NUI",
+	"곤":    "GON",
+	"키프로사": "KYPROSA",
+}
+
 func form(m map[string]string) io.Reader {
 	data := url.Values{}
 	for k, v := range m {
