be64ec5cd418821ab18675872ec7795888e80e6b
Jongyeol Baek <geeksbaek@gmail.com>
2017-07-21 21:30:12 +0900

    update notice test code

--- notice_test.go
+++ notice_test.go
@@ -1,14 +1,20 @@
 package archeage
 
 import (
+	"errors"
 	"net/http"
 	"testing"
 )
 
 func TestFetchNotice(t *testing.T) {
 	aa := New(&http.Client{})
-	_, err := aa.FetchNotice()
+	ns, err := aa.FetchNotice()
 	if err != nil {
 		t.Error(err)
+	}
+	for _, v := range ns {
+		if v.Title == "" {
+			t.Error(errors.New("Cannot Parse Notice Title"))
+		}
 	}
 }
