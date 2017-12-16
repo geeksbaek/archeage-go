0fe3e905a9384d0bc8801586717b45a8f9fc0fc6
Jongyeol Baek <geeksbaek@gmail.com>
2017-12-17 02:54:50 +0900

    update auction_test

--- auction_test.go
+++ auction_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestAction(t *testing.T) {
 	aa := New(&http.Client{})
-	_, err := aa.Auction("TOTAL", "목재")
+	_, err := aa.Auction("TOTAL", "목재", 1)
 	if err != nil {
 		t.Error(err)
 	}
